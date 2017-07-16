25e6295c8bf1762730c5f10cd9927244f9d0458e
Narcieliton Lopes <narcieliton@hotmail.com>
2017-07-16 20:14:57 -0300

    Initial commit

---
+++ app/src/main/java/com/narcielitonlopes/conversormoedas/MainActivity.java
@@ -0,0 +1,33 @@
+package com.narcielitonlopes.conversormoedas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = (EditText) findViewById(R.id.edit_value);
+        this.mViewHolder.textDollar = (TextView) findViewById(R.id.text_dollar);
+        this.mViewHolder.textEuro = (TextView) findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = (Button) findViewById(R.id.button_calculate);
+
+    }
+
+    private static class ViewHolder{
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttonCalculate;
+    }
+}
