0ebfed4a3ce13371d3df106edb90a6c168e5c2c4
Narcieliton Lopes <narcieliton@hotmail.com>
2017-07-17 21:32:39 -0300

    convertendo valores com euro e dollar ficticios para testes.

--- app/src/main/java/com/narcielitonlopes/conversormoedas/MainActivity.java
+++ app/src/main/java/com/narcielitonlopes/conversormoedas/MainActivity.java
@@ -2,13 +2,14 @@ package com.narcielitonlopes.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -22,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textEuro = (TextView) findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = (Button) findViewById(R.id.button_calculate);
 
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+
+        this.clearValues();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.button_calculate){
+           Double value = Double.valueOf(this.mViewHolder.editValue.getText().toString());
+           this.mViewHolder.textDollar.setText(String.format("%.2f", value * 3));
+           this.mViewHolder.textEuro.setText(String.format("%.2f", value * 4));
+        }
+    }
+
+    private void clearValues(){
+        this.mViewHolder.textDollar.setText("");
+        this.mViewHolder.textEuro.setText("");
     }
 
     private static class ViewHolder{
